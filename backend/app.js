d3d8b57de806f90e548440733d038bae7fc81d62
Erick Kahinga <erickkahingainfo@gmail.com>
2023-10-07 02:20:35 -0400

    Created a route and created db template

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-// This is the middleware
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
+// Routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
 const server = () => {
-    console.log("Testing access: ", PORT);
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port: ', PORT);
+    })
 }
 
 server();
