953bee2a2e66841af949a2b87e4a351a6c877a6d
Erick Kahinga <erickkahingainfo@gmail.com>
2023-10-07 06:02:59 -0400

    Post, Get and Delete have been created to manipulate income in the database

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,9 @@
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
-})
+router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome);
 
 module.exports = router
