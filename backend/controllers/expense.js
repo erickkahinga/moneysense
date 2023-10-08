84336122c26e1378d060640a971256289ffd453b
Erick Kahinga <erickkahingainfo@gmail.com>
2023-10-07 20:57:43 -0400

    End points for get, add, and delete for both income and expense tracking is complete

---
+++ backend/controllers/expense.js
@@ -0,0 +1,52 @@
+const ExpenseSchema = require("../models/ExpenseModel")
+
+
+exports.addExpense = async (req, res) => {
+    const {title, amount, category, description, date} = req.body
+
+    const expense =ExpenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try {
+        // validations
+        if (!title || !category || !description || !date) {
+            return res.status(400).json({message: 'Do not leave any field blank'})
+        }
+
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json({message: 'Amount must be a number > 0'})
+        }
+
+        await expense.save()
+        res.status(200).json({message: 'Expense Added'})
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+
+    console.log(expense)
+}
+
+exports.getExpenses = async (req, res) => {
+    try {
+        const expenses = await ExpenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(expenses)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.deleteExpense = async (req, res) => {
+    const {id} = req.params;
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((expense) => {
+            res.status(200).json({message: 'Expense Deleted'})
+        })
+        .catch((error) => {
+            res.status(500).json({message: 'Server Error : '})
+        })
+}
